ad51a98bf79e402f15d888655ee54ba9e1362df7
mohitpanchal09 <90472961+mohitpanchal09@users.noreply.github.com>
2023-07-08 14:31:38 +0530

    Update FavIconProvider.tsx

--- components/Misc/FavIconProvider.tsx
+++ components/Misc/FavIconProvider.tsx
@@ -6,14 +6,7 @@ const FavIconProvider = ({ children }: { children: JSX.Element }) => {
   return (
     <>
       <Head>
-        <link
-          rel="icon"
-          href={
-            colorMode === 'dark' ? '/favicon.ico' : '/favicon-lightmode.ico'
-          }
-        />
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+       
       </Head>
       {children}
     </>
