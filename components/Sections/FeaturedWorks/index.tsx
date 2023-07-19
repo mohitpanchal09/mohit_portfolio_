41504c3671b5a20bcbddf3f75831af7415d4aa8c
mohitpanchal09 <90472961+mohitpanchal09@users.noreply.github.com>
2023-07-19 12:02:35 +0530

    Updated new project switch shop

--- components/Sections/FeaturedWorks/index.tsx
+++ components/Sections/FeaturedWorks/index.tsx
@@ -43,34 +43,20 @@ const FeaturedWorksSection = () => {
         <MotionGridItem colSpan={6} variants={fadeInUpSlower}>
           <FeaturedCard
             idx={1}
-            title="Scholorly"
-            src="https://media.discordapp.net/attachments/935131235230023730/1126917544285057135/scholarly.png?width=795&height=558"
-            description="Web-based platform that allows users to search for national and international scholarships from various providers and recommend them scholarships based on their interests, providing a convenient and efficient solution for education needs. Also won Smart India Hackathon for this project and recieved a prize money of Rs.1,00,000
-
-            "
+            title="Switch shop"
+            description="Built an Ecommerce website using MERN stack. Implemented user authentication and CRUD operations to allow users to create and manage 
+            profiles. Used JSON web token (JWT) for authorization.
+            Also built an Admin panel to add and delete products. It also includes analytics for sales, revenue and total active users"
+            src="https://media.discordapp.net/attachments/935131235230023730/1131110809888628746/Screenshot_26.png?width=1251&height=606"
             height={{ base: '130px', md: '225px', '2xl': '300px' }}
-            ctaUrl="https://github.com/mohitpanchal09/scholorship_portal.git"
-            objectPosition="right 20%"
+            ctaUrl="https://switchshop.netlify.app/"
             isMobile={isMobile}
           />
         </MotionGridItem>
-
-        <MotionGridItem colSpan={6} variants={fadeInUpSlower}>
-          <FeaturedCard
-            idx={2}
-            title="Hackathon website"
-            description="Created a website to host a hackathon event for college students, with features such as registration, event schedules, submission forms, and judging criteria. It will involve front-end and back-end development, and integration with third-party APIs."
-            src="https://media.discordapp.net/attachments/935131235230023730/1126918749585424545/hackathon_website.png?width=967&height=558"
-            height={{ base: '130px', md: '225px', '2xl': '300px' }}
-            ctaUrl="https://hackx-niet.tech/"
-            isMobile={isMobile}
-          />
-        </MotionGridItem>
-
         <MotionGridItem colSpan={6} variants={fadeInUpSlower}>
           <FeaturedCard
           
-            idx={3}
+            idx={2}
             title="Careerbuddy"
             description="Designed the front-end UI for the recommendation system using Figma.
             Built a career guidance portal leveraged with reusable components, recommendation system, meeting scheduling with mentor, resume parser, mentor rating 
@@ -83,6 +69,36 @@ const FeaturedWorksSection = () => {
             isMobile={isMobile}
           />
         </MotionGridItem>
+        
+
+        <MotionGridItem colSpan={6} variants={fadeInUpSlower}>
+          <FeaturedCard
+            idx={3}
+            title="Hackathon website"
+            description="Created a website to host a hackathon event for college students, with features such as registration, event schedules, submission forms, and judging criteria. It will involve front-end and back-end development, and integration with third-party APIs."
+            src="https://media.discordapp.net/attachments/935131235230023730/1126918749585424545/hackathon_website.png?width=967&height=558"
+            height={{ base: '130px', md: '225px', '2xl': '300px' }}
+            ctaUrl="https://hackx-niet.tech/"
+            isMobile={isMobile}
+          />
+        </MotionGridItem>
+        <MotionGridItem colSpan={6} variants={fadeInUpSlower}>
+        <FeaturedCard
+            idx={4}
+            title="Scholorly"
+            src="https://media.discordapp.net/attachments/935131235230023730/1126917544285057135/scholarly.png?width=795&height=558"
+            description="Web-based platform that allows users to search for national and international scholarships from various providers and recommend them scholarships based on their interests, providing a convenient and efficient solution for education needs. Also won Smart India Hackathon for this project and recieved a prize money of Rs.1,00,000
+
+            "
+            height={{ base: '130px', md: '225px', '2xl': '300px' }}
+            ctaUrl="https://github.com/mohitpanchal09/scholorship_portal.git"
+            objectPosition="right 20%"
+            isMobile={isMobile}
+          />
+        </MotionGridItem>
+        
+
+        
 
         {/* <MotionGridItem colSpan={6} variants={fadeInUpSlower}>
           <FeaturedCard
