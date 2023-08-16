34bba821657daf6d511c103b5c63d2d5d8b75039
mohitpanchal09 <90472961+mohitpanchal09@users.noreply.github.com>
2023-08-16 11:51:51 +0530

    Update OpenGraphHead.tsx

--- components/Misc/OpenGraphHead.tsx
+++ components/Misc/OpenGraphHead.tsx
@@ -2,11 +2,10 @@ import Head from 'next/head'
 const OpenGraphHead = () => (
   <Head>
     <title>Mohit Kumar</title>
-    <meta name="description" content="My personal solace place on web-earth." />
+    <meta name="description" content="Mohit Panchal, expertise in Fronten development. Talks about Development,Technologies. Expert in HTML CSS JS REACTJS MONGODB NODEJS EXPRESSJS and PYTHON" />
     <meta property="og:title" content="Mohit Kumar" />
-    <meta property="og:site_name" content="Mohit Panchal, expertise in Fronten development. Talks about Development,Technologies. Expert in HTML CSS JS REACTJS MONGODB NODEJS EXPRESSJS and PYTHON" />
+    <meta property="og:site_name" content="Mohit Panchal" />
     
-    <meta property="og:type" content="profile" />
     
   </Head>
 )
